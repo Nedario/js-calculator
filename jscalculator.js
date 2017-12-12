d53c0980528793775378203ad0b80836e491af6c
Nedario <khalil.awad2@gmail.com>
2017-12-12 19:46:34 +0100

    recette v1.1

--- jscalculator.js
+++ jscalculator.js
@@ -1,5 +1,6 @@
 var btn = document.getElementById('button');
 var n = document.getElementById('result');
+var tab = [];
 
 function recherche(nb1, nb2, sign){
 var calcul = new Object();
@@ -15,30 +16,54 @@ function calculer(obj){
     return eval(obj.input1 + obj.sigle + obj.input2);
 };
 
-function afficher(res){   
-    if(!isFinite(res))
-        n.textContent = "Erreur de calcul";
-    else
+function afficher(res){
         n.textContent = res;
 };
 
-function verifSaisie(obj){
-    if(!isNaN(obj.input1) && !isNaN(obj.input2)){
+function tabErrors(obj, res){
+    // if(!isNaN(obj.input1) || !isNaN(obj.input2)){
+        if(!isNaN(obj.input1))
+            tab.push("La premiere saisie n'est pas un nombre")
+        if(!isNaN(obj.input2))
+            tab.push("La deuxieme saisie n'est pas un nombre")
+        if(!isFinite(res))
+            tab.push("Erreur de calcul");
+        return tab;
+    // }else{
+    //     return false;
+    // }
+    // return tab;
+}
+function verifErrors(){
+    if(tab.length>0)
         return true;
-    }else{
+    else
         return false;
+}
+
+function afficherErrors(){
+    for(i=0;i<tab.length;i++){
+        n.innerHTML = tab[i];
     }
 };
 
-function init(){
-    var a = recherche("nb1","nb2","selector");
-    var errors = verifSaisie(a);
-    if(errors){
-        var b = calculer(a);
-        afficher(b);
-    }else{
-        n.textContent = "Veuillez n'entrer que des nombres";
-    }
-};
+// function verifSaisie(obj){
+//     if(!isNaN(obj.input1) && !isNaN(obj.input2)){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// };
 
-btn.addEventListener('click', init);
+function main(){
+
+    var saisie = recherche("nb1","nb2","selector");
+    var resultat = calculer(saisie);
+    tabErrors(saisie, resultat);
+    if(verifErrors())
+        afficherErrors();
+    else
+        afficher(resultat);
+}
+
+btn.addEventListener('click', main);
